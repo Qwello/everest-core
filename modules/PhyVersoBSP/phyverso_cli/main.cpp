0cb18c836dd5f726893a9afadf26e2020f4d245c
Dima Dorezyuk <ddo@qwello.eu>
2024-01-26 12:58:36 +0100

    PhyVersoBSP: change namings

--- modules/PhyVersoBSP/phyverso_cli/main.cpp
+++ modules/PhyVersoBSP/phyverso_cli/main.cpp
@@ -131,8 +131,6 @@ int main(int argc, char* argv[]) {
 
         p.signal_opaque_data.connect([](int connector, const std::vector<int32_t>& data) {
             EVLOG_info << "Received data from " << connector;
-            for (const auto a : data)
-                EVLOG_info << a;
         });
 
         while (true) {
