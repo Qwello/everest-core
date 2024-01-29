4a15048b882bcf226f96a3d9da0c50eeb038e75a
Dima Dorezyuk <ddo@qwello.eu>
2024-01-29 11:19:38 +0100

    formatting

--- modules/PhyVersoBSP/phyverso_cli/main.cpp
+++ modules/PhyVersoBSP/phyverso_cli/main.cpp
@@ -129,9 +129,8 @@ int main(int argc, char* argv[]) {
             }
         });
 
-        p.signal_opaque_data.connect([](int connector, const std::vector<int32_t>& data) {
-            EVLOG_info << "Received data from " << connector;
-        });
+        p.signal_opaque_data.connect(
+            [](int connector, const std::vector<int32_t>& data) { EVLOG_info << "Received data from " << connector; });
 
         while (true) {
             char c = getc(stdin);
