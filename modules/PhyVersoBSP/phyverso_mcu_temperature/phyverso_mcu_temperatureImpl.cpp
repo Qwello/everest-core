2d514760c06781a73881e2e45a7798d782d33029
ju4nlu <juanluisgomezchanclon@gmail.com>
2024-04-16 08:28:33 +0200

    Use correct name for temperature logger implementation (#49)
    * Use correct name for temperature logger implementation
    
    * fix the build
    
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    
    * fix bazel
    
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    
    ---------
    
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    Co-authored-by: juanlu <jgo@qwello.eu>
    Co-authored-by: Dima Dorezyuk <ddo@qwello.eu>

---
+++ modules/PhyVersoBSP/phyverso_mcu_temperature/phyverso_mcu_temperatureImpl.cpp
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Pionix GmbH and Contributors to EVerest
+
+#include "phyverso_mcu_temperatureImpl.hpp"
+
+namespace module {
+namespace phyverso_mcu_temperature {
+
+void phyverso_mcu_temperatureImpl::init() {
+}
+
+void phyverso_mcu_temperatureImpl::ready() {
+}
+
+} // namespace phyverso_mcu_temperature
+} // namespace module
